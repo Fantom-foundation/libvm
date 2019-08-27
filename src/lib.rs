b9cc3cb24fee68977329691f164e87fd88ee1a2e
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-08-27 06:22:22 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use failure::Error;
+
+pub trait Instruction {
+    fn size(&self) -> Result<usize, Error>;
+    fn get_cycles(&self) -> Result<usize, Error>;
+}
+
+pub trait Cpu<I>
+where
+    I: Instruction,
+{
+    fn execute(&mut self) -> Result<usize, Error> {
+        if !self.can_run() || self.is_done() {
+            return Ok(0);
+        }
+        let instruction = self
+            .get_next_instruction()
+            .unwrap();
+        self.increase_pc(instruction.size()?);
+        let cycles = self.get_cycles_for_instruction(&instruction)?;
+        self.execute_instruction(instruction)?;
+        Ok(cycles)
+    }
+    fn get_cycles_for_instruction(&mut self, instruction: &I) -> Result<usize, Error> {
+        instruction.get_cycles()
+    }
+    fn execute_instruction(&mut self, instruction: I) -> Result<(), Error>;
+    fn get_pc(&self) -> usize;
+    fn get_next_instruction(&mut self) -> Option<I>;
+    fn can_run(&self) -> bool;
+    fn is_done(&self) -> bool;
+    fn increase_pc(&mut self, steps: usize);
+}

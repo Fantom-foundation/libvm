cc284d48a3567322a62d146b14f6abc4b46791cb
Victor <shdownnine@gmail.com>
2019-10-29 22:16:44 +0300

    src/lib.rs: try to fix building with BUILD_FMT=1

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ where
     fn can_run(&self) -> bool;
     fn is_done(&self) -> bool;
     fn increase_pc(&mut self, steps: usize);
-    fn set_instructions<J: Iterator<Item=I>>(&mut self, i: J, sender: P);
+    fn set_instructions<J: Iterator<Item = I>>(&mut self, i: J, sender: P);
 }
 
 pub trait DistributedVM<'d, C, I, D, A, P>
